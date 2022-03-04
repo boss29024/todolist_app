ac4a1a80c64196fb32677603f055f07058da132c
boss29024 <boss29024@gmail.com>
2022-03-04 14:39:06 +0700

    คอมเม้นส่วนที่ไม่ใช่

--- to_do_list/lib/widgets/cardAllSchedule.dart
+++ to_do_list/lib/widgets/cardAllSchedule.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
 
-class CardAllSchedule extends StatefulWidget {
-  const CardAllSchedule({Key? key}) : super(key: key);
+class CardAllSchedule extends StatelessWidget {
+  const CardAllSchedule({
+    Key? key,
+    //this.onChanged,
+    //this.onTap,
+    //this.readOnly = false,
+    this.width = 1,
+    this.height = 1,
+    //this.decoration,
+    // this.name,
+    // this.comment,
+    this.score = 0,
+  }) : super(key: key);
 
-  @override
-  State<CardAllSchedule> createState() => _CardAllScheduleState();
-}
+  //final Function onChanged;
+  //final Function onTap;
+  //final bool readOnly;
+  final double width;
+  final double height;
+  //final InputDecoration decoration;
+  // final String name;
+  // final String comment;
+  final double score;
 
-class _CardAllScheduleState extends State<CardAllSchedule> {
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
+    final widthDevice = MediaQuery.of(context).size.width;
+    final heightDevice = MediaQuery.of(context).size.height;
+        return InkWell(
+            //onTap: this.onTap,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Container(
+                height: (heightDevice / 100) * 18,
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: (heightDevice / 100) * 1,
+                              ),
+                              SizedBox(
+                                height: (heightDevice / 100) * 3,
+                              ),
+                              SizedBox(
+                                height: (heightDevice / 100) * 3,
+                              ),
+                            ],
+                          )),
+                      Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height: (heightDevice / 100) * 1,
+                              ),
+                            ],
+                          ))
+                    ]),
+              ),
+            ));
+      }}
