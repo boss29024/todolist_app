b71f0d41b52c0ea0f4cad821380b57e118a6360e
boss29024 <boss29024@gmail.com>
2022-03-04 15:19:30 +0700

    AppBar

--- to_do_list/lib/page/HomePage.dart
+++ to_do_list/lib/page/HomePage.dart
@@ -13,12 +13,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('To do list'),
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+        ],
+      ),
       // ignore: avoid_unnecessary_containers
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(
-            children: const [Text('ค้นหา')],
+            children: const [Text('')],
           ),
         ),
       ),
