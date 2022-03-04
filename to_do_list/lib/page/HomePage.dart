b876a4babb2718291833efc4ee560bc69352d8a8
boss29024 <boss29024@gmail.com>
2022-03-04 13:40:41 +0700

    Container

--- to_do_list/lib/page/HomePage.dart
+++ to_do_list/lib/page/HomePage.dart
@@ -1,41 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key, }) : super(key: key);
-
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(),
+        // ignore: avoid_unnecessary_containers
+        body: Container(
+          child: const Text(''),
+        ));
   }
 }
