3d508d1e978cb612f5c0944f9c5992d1eca78be1
boss29024 <boss29024@gmail.com>
2022-03-07 13:33:52 +0700

    ListForm

---
+++ to_do_list/lib/widgets/listForm.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListForm extends StatefulWidget {
+  ListForm({Key? key}) : super(key: key);
+
+  @override
+  State<ListForm> createState() => _ListFormState();
+}
+
+void doNothing(BuildContext context) {}
+
+class _ListFormState extends State<ListForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
