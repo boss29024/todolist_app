8b7f50b21d08a1f2a3371e0fe0630001d7034f4a
boss29024 <boss29024@gmail.com>
2022-03-07 16:00:55 +0700

    FloatingActionButton

--- to_do_list/lib/page/HomePage.dart
+++ to_do_list/lib/page/HomePage.dart
@@ -14,6 +14,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // final widthDevice = MediaQuery.of(context).size.width;
@@ -46,10 +58,15 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   SlidableWidgets(),
                 ],
-              )
+              ),
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
