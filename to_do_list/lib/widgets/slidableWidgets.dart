5cad27ec1e9fda9ee6c8318d607ba391aee97aa5
boss29024 <boss29024@gmail.com>
2022-03-07 13:21:19 +0700

    Slidable

---
+++ to_do_list/lib/widgets/slidableWidgets.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class SlidableWidgets extends StatefulWidget {
+  SlidableWidgets({Key? key}) : super(key: key);
+
+  @override
+  State<SlidableWidgets> createState() => _SlidableWidgetsState();
+}
+
+void doNothing(BuildContext context) {}
+
+class _SlidableWidgetsState extends State<SlidableWidgets> {
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      // Specify a key if the Slidable is dismissible.
+      key: const ValueKey(0),
+
+      // The start action pane is the one at the left or the top side.
+      startActionPane: ActionPane(
+        // A motion is a widget used to control how the pane animates.
+        motion: const ScrollMotion(),
+
+        // A pane can dismiss the Slidable.
+        dismissible: DismissiblePane(onDismissed: () {}),
+
+        // All actions are defined in the children parameter.
+        children: const [
+          // A SlidableAction can have an icon and/or a label.
+          SlidableAction(
+            onPressed: doNothing,
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            // An action can be bigger than the others.
+            onPressed: doNothing,
+            backgroundColor: Color(0xFF7BC043),
+            foregroundColor: Colors.white,
+            icon: Icons.error_outline,
+            label: 'Correct',
+          ),
+        ],
+      ),
+
+      // The end action pane is the one at the right or the bottom side.
+      endActionPane: const ActionPane(
+        motion: ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            onPressed: doNothing,
+            backgroundColor: Color(0xFF7BC043),
+            foregroundColor: Colors.white,
+            icon: Icons.error_outline,
+            label: 'Correct',
+          ),
+          SlidableAction(
+            onPressed: doNothing,
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+
+      // The child of the Slidable is what the user sees when the
+      // component is not dragged.
+      child: const ListTile(title: Text('Slide me')),
+    );
+  }
+}
