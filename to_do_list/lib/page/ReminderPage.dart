e4dd8c09527cb2424760f6de0b6e8a318e5598de
boss29024 <boss29024@gmail.com>
2022-03-08 14:21:53 +0700

    ReminderPage

---
+++ to_do_list/lib/page/ReminderPage.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class ReminderPage extends StatefulWidget {
+  ReminderPage({
+    Key? key,
+  }) : super(key: key);
+
+  // final String listName;
+  // final IconData iconList;
+
+  @override
+  State<ReminderPage> createState() => _ReminderPageState();
+}
+
+class _ReminderPageState extends State<ReminderPage> {
+  @override
+  Widget build(BuildContext context) {
+    final heightDevice = MediaQuery.of(context).size.height;
+    final widthDevice = MediaQuery.of(context).size.width;
+    return Container(
+      child: InkWell(
+          //onTap: this.onTap,
+          child: Column(
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Container(
+              height: (heightDevice / 100) * 18,
+              width: (widthDevice / 100) * 45,
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: (heightDevice / 100) * 2,
+                            ),
+                            Icon(Icons.add),
+                            SizedBox(
+                              height: (heightDevice / 100) * 4,
+                            ),
+                            const TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'ชื่อลิสต์'),
+                            ),
+                            //Text(listName)
+                          ],
+                        )),
+                  ]),
+            ),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Container(
+              height: (heightDevice / 100) * 18,
+              width: (widthDevice / 100) * 45,
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Wrap(
+                              direction: Axis.vertical,
+                              spacing: 7,
+                              runSpacing: 10,
+                              children: [
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                                Icon(Icons.add),
+                              ],
+                            )
+                          ],
+                        )),
+                  ]),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
