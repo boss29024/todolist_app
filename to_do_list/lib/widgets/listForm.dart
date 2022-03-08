6080a38c1f27d120b07c10f3ebd7a204ec782c1c
boss29024 <boss29024@gmail.com>
2022-03-08 10:06:16 +0700

    เปลี่ยนstateful เป็น stateless

--- to_do_list/lib/widgets/listForm.dart
+++ to_do_list/lib/widgets/listForm.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
-class ListForm extends StatefulWidget {
-  ListForm({Key? key}) : super(key: key);
+class ListForm extends StatelessWidget {
+  const ListForm({Key? key}) : super(key: key);
 
-  @override
-  State<ListForm> createState() => _ListFormState();
-}
-
-void doNothing(BuildContext context) {}
-
-class _ListFormState extends State<ListForm> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final heightDevice = MediaQuery.of(context).size.height;
+    final widthDevice = MediaQuery.of(context).size.width;
+    return InkWell(
+        //onTap: this.onTap,
+        child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Container(
+        height: (heightDevice / 100) * 18,
+        width: (widthDevice / 100) * 45,
+        padding: const EdgeInsets.all(10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: (heightDevice / 100) * 2,
+                      ),
+                      SizedBox(
+                        height: (heightDevice / 100) * 4,
+                      ),
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: (heightDevice / 100) * 2,
+                      ),
+                    ],
+                  )),
+            ]),
+      ),
+    ));
   }
 }
