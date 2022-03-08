e4018f70a5678563f1aac17db7085caf4e26a6ef
boss29024 <boss29024@gmail.com>
2022-03-08 15:54:00 +0700

    ใช่ชือลิสต์

--- to_do_list/lib/page/FormListPage.dart
+++ to_do_list/lib/page/FormListPage.dart
@@ -19,7 +19,6 @@ class _FormListPageState extends State<FormListPage> {
     final widthDevice = MediaQuery.of(context).size.width;
     return Container(
       child: InkWell(
-          //onTap: this.onTap,
           child: Column(
         children: [
           Card(
@@ -42,65 +41,16 @@ class _FormListPageState extends State<FormListPage> {
                             SizedBox(
                               height: (heightDevice / 100) * 2,
                             ),
-                            Icon(Icons.add),
-                            SizedBox(
-                              height: (heightDevice / 100) * 4,
-                            ),
                             const TextField(
                               decoration: InputDecoration(
                                   border: OutlineInputBorder(),
                                   hintText: 'ชื่อลิสต์'),
                             ),
-                            //Text(listName)
                           ],
                         )),
                   ]),
             ),
           ),
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Container(
-              height: (heightDevice / 100) * 18,
-              width: (widthDevice / 100) * 45,
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                        flex: 1,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Wrap(
-                              direction: Axis.vertical,
-                              spacing: 7,
-                              runSpacing: 10,
-                              children: [
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                                Icon(Icons.add),
-                              ],
-                            )
-                          ],
-                        )),
-                  ]),
-            ),
-          )
         ],
       )),
     );
