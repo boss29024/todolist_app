0622c17c3d6ee7c2afe1d1abd9a07d56e1cfd488
boss29024 <boss29024@gmail.com>
2022-03-09 15:08:22 +0700

    Detailsbottom

--- to_do_list/lib/widgets/detailsBottom.dart
+++ to_do_list/lib/widgets/detailsBottom.dart
@@ -13,41 +13,40 @@ class Detailsbottom extends StatelessWidget {
   Widget build(BuildContext context) {
     final heightDevice = MediaQuery.of(context).size.height;
     final widthDevice = MediaQuery.of(context).size.width;
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Container(
-              height: (heightDevice / 100) * 10,
-              width: widthDevice,
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                        child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [Icon(icon), Text(headname)],
-                    )),
-                    Expanded(
-                        child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        SwitcherButton(
-                          value: true,
-                          onChange: (value) {},
-                        )
-                      ],
-                    )),
-                  ]),
-            ),
-          ),
-        ],
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Container(
+        height: (heightDevice / 100) * 10,
+        width: widthDevice,
+        padding: const EdgeInsets.all(10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(icon),
+                  SizedBox(
+                    width: (widthDevice / 100) * 1,
+                  ),
+                  Text(headname)
+                ],
+              )),
+              Container(
+                  child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  SwitcherButton(
+                    value: false,
+                    onChange: (value) {},
+                  )
+                ],
+              )),
+            ]),
       ),
     );
   }
