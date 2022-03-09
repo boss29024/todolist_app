a220be2ae405a08b80488a2606ddc0cc2e3d6b71
boss29024 <boss29024@gmail.com>
2022-03-09 15:26:50 +0700

    ListPage

---
+++ to_do_list/lib/widgets/listPageWidget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ทั้งหมด'),
+      ),
+      body: Container(
+        child: Column(children: []),
+      ),
+    );
+  }
+}
