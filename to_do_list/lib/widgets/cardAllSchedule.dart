4eca7fa9be8e48e00b8dc13b2eca270fd83b7723
boss29024 <boss29024@gmail.com>
2022-03-10 13:35:00 +0700

    ลบInkWell

--- to_do_list/lib/widgets/cardAllSchedule.dart
+++ to_do_list/lib/widgets/cardAllSchedule.dart
@@ -21,50 +21,46 @@ class CardAllSchedule extends StatelessWidget {
     final heightDevice = MediaQuery.of(context).size.height;
     final widthDevice = MediaQuery.of(context).size.width;
 
-    return InkWell(
-        onTap: () {
-          Navigator.pushNamed(context, '/listtoday');
-        },
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: Container(
-            height: (heightDevice / 100) * 18,
-            width: (widthDevice / 100) * 45,
-            padding: const EdgeInsets.all(10),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: (heightDevice / 100) * 2,
-                          ),
-                          Icon(icon),
-                          SizedBox(
-                            height: (heightDevice / 100) * 4,
-                          ),
-                          Text(headcard),
-                        ],
-                      )),
-                  Expanded(
-                      flex: 1,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          SizedBox(
-                            height: (heightDevice / 100) * 2,
-                          ),
-                          Text('$score', style: const TextStyle(fontSize: 30)),
-                        ],
-                      )),
-                ]),
-          ),
-        ));
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Container(
+        height: (heightDevice / 100) * 18,
+        width: (widthDevice / 100) * 45,
+        padding: const EdgeInsets.all(10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: (heightDevice / 100) * 2,
+                      ),
+                      Icon(icon),
+                      SizedBox(
+                        height: (heightDevice / 100) * 4,
+                      ),
+                      Text(headcard),
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: (heightDevice / 100) * 2,
+                      ),
+                      Text('$score', style: const TextStyle(fontSize: 30)),
+                    ],
+                  )),
+            ]),
+      ),
+    );
   }
 }
