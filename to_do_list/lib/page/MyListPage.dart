314597f2af4a93be1a4183a30131012b0147baea
boss29024 <boss29024@gmail.com>
2022-03-14 16:31:35 +0700

    MyListPage

--- to_do_list/lib/page/MyListPage.dart
+++ to_do_list/lib/page/MyListPage.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/widgets/listWidget.dart';
 
-class ListPage extends StatefulWidget {
-  ListPage({Key? key}) : super(key: key);
+class MyListPage extends StatefulWidget {
+  MyListPage({Key? key}) : super(key: key);
 
   @override
-  State<ListPage> createState() => _ListPageState();
+  State<MyListPage> createState() => _MyListPageState();
 }
 
-class _ListPageState extends State<ListPage> {
+class _MyListPageState extends State<MyListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
