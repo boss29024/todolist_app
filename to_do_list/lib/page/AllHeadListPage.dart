b40eebf02a78b343efe19f5186f94e5293fda11e
boss29024 <boss29024@gmail.com>
2022-03-15 10:10:55 +0700

    getx

--- to_do_list/lib/page/AllHeadListPage.dart
+++ to_do_list/lib/page/AllHeadListPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:to_do_list/widgets/listWidget.dart';
+
+import '../controllers/todo.controller.dart';
 
 class AllHeadListPage extends StatefulWidget {
   AllHeadListPage({Key? key}) : super(key: key);
@@ -11,23 +15,25 @@ class _AllHeadListPageState extends State<AllHeadListPage> {
   bool value = false;
   @override
   Widget build(BuildContext context) {
+    TodoController todoController = Get.find();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ลิสต์ทั้งหมด'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(12),
-        children: [
-          CheckboxListTile(
-              title: Text('การเตือน'),
-              value: value,
-              onChanged: (value) {
-                setState(() {
-                  this.value = value!;
-                });
-              })
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('ลิสต์ทั้งหมด'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(12),
+          child: Obx(() => ListView.builder(
+              itemCount: todoController.todos.length,
+              itemBuilder: (_buildContext, index) {
+                return CheckboxListTile(
+                    title: Text('การเตือน'),
+                    value: value,
+                    onChanged: (value) {
+                      setState(() {
+                        this.value = value!;
+                      });
+                    });
+              })),
+        ));
   }
 }
