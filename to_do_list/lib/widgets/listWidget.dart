8c2ece6ee5e963ce8aeb2081a5ca2509745a005e
boss29024 <boss29024@gmail.com>
2022-03-16 14:27:13 +0700

    เปลี่ยนStateless เป็นStateful

--- to_do_list/lib/widgets/listWidget.dart
+++ to_do_list/lib/widgets/listWidget.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
 
-class ListWidget extends StatelessWidget {
-  ListWidget(
-      {Key? key,
-      this.headTitle = '',
-      this.detailTitle = '',
-      this.date = '',
-      this.time = ''})
-      : super(key: key);
-
+class ListWidget extends StatefulWidget {
   final String headTitle;
   final String detailTitle;
   final String date;
   final String time;
 
+  const ListWidget({
+    Key? key,
+    required this.headTitle,
+    required this.detailTitle,
+    required this.date,
+    required this.time,
+  }) : super(key: key);
+
+  @override
+  State<ListWidget> createState() => _ListWidgetState();
+}
+
+class _ListWidgetState extends State<ListWidget> {
+  bool value = false;
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
-        title: Text(headTitle),
-        subtitle: Text(detailTitle),
+        title: Text(
+          widget.headTitle,
+        ),
+        subtitle: Text(widget.headTitle),
         children: <Widget>[
           ListTile(
-            title: Text('วันที่ $date'),
+            title: Text('วันที่ ${widget.date}'),
           ),
-          ListTile(title: Text('เวลา $time')),
-          ListTile(
-            title: ElevatedButton(
-              onPressed: () {},
-              child: Text('ทำเสร็จแล้ว'),
-            ),
-          )
+          ListTile(title: Text('เวลา ${widget.time}')),
+          CheckboxListTile(
+            value: value,
+            onChanged: (value) {
+              setState(() {
+                this.value = value!;
+              });
+            },
+            title: Text('ทำเสร็จแล้ว'),
+          ),
         ]);
   }
 }
