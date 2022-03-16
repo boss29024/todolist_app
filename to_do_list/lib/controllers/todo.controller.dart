1ece0f9dea2b6d0a5786d7d6b7779723317bdf86
boss29024 <boss29024@gmail.com>
2022-03-16 16:41:26 +0700

    editTodo

--- to_do_list/lib/controllers/todo.controller.dart
+++ to_do_list/lib/controllers/todo.controller.dart
@@ -9,9 +9,16 @@ class TodoController extends GetxController {
   var todos = <Todo>[].obs;
   var isLoading = false.obs;
   var errorMsg = "".obs;
- 
+
   void addTodo(Todo todo) {
     todos.value = [...todos, todo];
   }
-  
+
+  void editTodo(int index, Todo todo) {
+    todos.value = [
+      ...todos.sublist(0, index),
+      todo,
+      ...todos.sublist(index + 1)
+    ];
+  }
 }
