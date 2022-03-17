92e248558d11490cc710ed7655287c7682e988d6
boss29024 <boss29024@gmail.com>
2022-03-17 12:22:13 +0700

    GetStorage เก็บข้อมูลลงเครื่อง deleteTodo

--- to_do_list/lib/controllers/todo.controller.dart
+++ to_do_list/lib/controllers/todo.controller.dart
@@ -6,9 +6,29 @@ import 'package:get_storage/get_storage.dart';
 import '../models/todo.dart';
 
 class TodoController extends GetxController {
+  GetStorage box = GetStorage();
   var todos = <Todo>[].obs;
   var isLoading = false.obs;
   var errorMsg = "".obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    var todosRaw = box.read('todos');
+    if (todosRaw != null) {
+      var json = jsonDecode(todosRaw.toString());
+      List<Todo> todoList =
+          List<Todo>.from(json.map((model) => Todo.fromJson(model))).toList();
+      todos.value = todoList;
+    }
+    ever(
+      todos,
+      (List<Todo> value) {
+        var jsonString = jsonEncode(value);
+        box.write('todos', jsonString);
+      },
+    );
+  }
 
   void addTodo(Todo todo) {
     todos.value = [...todos, todo];
@@ -20,5 +40,9 @@ class TodoController extends GetxController {
       todo,
       ...todos.sublist(index + 1)
     ];
+  }
+
+  void deleteTodo(int index, Todo todo) {
+    todos.value = [...todos.sublist(0, index), ...todos.sublist(index + 1)];
   }
 }
